a049cea76736cdc7a1034c2242b12160ac8fd343
Apurba Sundar Nayak <apurbasundar2002@gmail.com>
2024-09-12 10:58:58 +0530

    Added input Elevation and Cadence changes.

--- script.js
+++ script.js
@@ -11,19 +11,51 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(position => {
-    const { latitude, longitude } = position.coords;
-    const coordinates= [latitude, longitude];
-    console.log(latitude);
-    console.log(longitude);
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    const map = L.map('map').setView(coordinates, 13);
+let map,mapEvent;
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+        const {latitude, longitude} = position.coords;
+        const coordinates = [latitude, longitude];
+        console.log(latitude);
+        console.log(longitude);
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        map = L.map('map').setView(coordinates, 13);
 
-    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    L.marker(coordinates).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        //handling clicks on Map
+        map.on('click', function (mapE) {
+            mapEvent = mapE;
+            form.classList.remove('hidden');
+            inputDistance.focus();
+        })
+    })
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //clear ip fields
+    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value =  '';
+
+
+    console.log(mapEvent);
+    const {lat,lng} = mapEvent.latlng;
+    L.marker([lat,lng]).addTo(map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            })
+        )
+        .setPopupContent('workout')
         .openPopup();
-})
+});
+
+inputType.addEventListener('change', (e) => {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
